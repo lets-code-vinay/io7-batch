53b6b233cdca4688a9f9d0a0842f86fc9906fb8e
Vinay Maurya <vinay.shermauryan@gmail.com>
2025-04-07 13:02:17 +0530

    20250407 js-2 datatype and variables

--- Daily class code/20250403 js-1/script.js
+++ Daily class code/20250403 js-1/script.js
@@ -155,3 +155,235 @@ let cd = ab + bc;
 console.log("the total of " + ab + " and " + bc + " is " + cd); // Not recommended
 console.log("the total of", ab, "and", bc, "is", cd);
 console.log(`the total of ${ab} and ${bc} is ${cd}`);
+
+// ------------------------ 2025-04-07 ----------------------------------------------
+
+// -------------------Assign operator ------------------------------------
+
+let studentName = "Chandni";
+var studentAge;
+
+var studentAge = 5;
+
+console.log("studentName--", studentName);
+console.log("studentAge--", studentAge);
+
+// -------------------Variable ------------------------------------
+
+/**
+ * Types of variable
+ * 1. let
+ * 2. Const
+ * 3. var
+ */
+
+/**
+ * VAR:
+ *  1. Old JS method to declare variable
+ *  2. Global scope variables
+ *  3. It hoisted the variable
+ */
+
+var mark = 10;
+
+console.log("mark first--", mark); // 10
+
+var mark = 50;
+console.log("mark above function--", mark); // 50
+
+function updateMark() {
+  var mark = 20;
+  console.log("mark inside function--", mark); // 20
+}
+
+updateMark();
+console.log("mark outside function--", mark); // 50/20
+
+function test() {
+  var a = 10;
+  if (true) {
+    var a = 20;
+    console.log("inside if testing var--", a);
+  }
+
+  console.log("inside if testing var-2--", a);
+}
+
+test();
+
+function test2() {
+  var a = 10;
+  function abc() {
+    var a = 20;
+    console.log("inside function testing var--", a);
+  }
+  abc();
+  console.log(" inside function  testing var-2--", a);
+}
+
+test2();
+
+/**
+ * LET:
+ *  1. Let introduced in ES6
+ *  2. local scope variables
+ *  3. It doesn't hoist the variable
+ */
+
+let price;
+console.log("printing price--", price);
+
+price = 20;
+console.log("printing price 2--", price);
+
+price = "Over priced";
+console.log("printing price 3--", price);
+
+let penPrice = 55;
+function pricing() {
+  console.log("printing penPrice inside function---", penPrice);
+
+  let price = 800;
+  console.log("printing price inside function 2----", price);
+
+  if (2 + 2 == 4) {
+    let price = 500;
+    console.log("printing price inside if condition----", price);
+  }
+  console.log("printing price inside function 2----", price);
+}
+
+pricing();
+
+function pricing2() {
+  let price = 800;
+  console.log("2- printing price inside function scope----", price);
+
+  function abc() {
+    let price = 500;
+    console.log("2-  printing price inside if condition----", price);
+  }
+  abc();
+  console.log("2- printing price inside function 2----", price);
+}
+
+pricing2();
+
+/**
+ * const:
+ *  1. const introduced in ES6
+ *  2. local scope variables
+ *  3. It doesn't hoist the variable
+ *  4. Can not reassign variable
+ */
+
+const mobileSize = "1024*1920";
+console.log("printing mobile size--", mobileSize);
+
+// mobileSize = "1024*1800";
+// console.log("printing mobile size--", mobileSize);
+/**
+ * It will throw an error
+ *
+ * Uncaught TypeError: Assignment to constant variable.
+ */
+
+function constPricing() {
+  console.log("3.- printing penPrice inside function---", penPrice);
+
+  const price = 800;
+  console.log("3.- printing price inside function 2----", price);
+
+  if (2 + 2 == 4) {
+    const price = 500;
+    console.log("3.- printing price inside if condition----", price);
+  }
+  console.log("3.- printing price inside function 2----", price);
+}
+
+constPricing();
+
+function constPricing2() {
+  const price = 800;
+  console.log("4.- printing price inside function scope----", price);
+
+  function abc() {
+    const price = 500;
+    console.log("4.-  printing price inside if condition----", price);
+  }
+  abc();
+  console.log("4.- printing price inside function 2----", price);
+}
+
+constPricing2();
+
+// /------------
+
+let bag = ["laptop", "mobile", "camera"];
+console.log("printing bag---", bag);
+
+bag = "i don't have bag";
+console.log("printing bag--2---", bag);
+
+const bag2 = ["laptop", "mobile", "camera"];
+console.log("printing bag-3--", bag2);
+
+// bag2 = "i don't have bag";
+// console.log("printing bag--2---", bag2);
+
+const newBag = bag2.with(2, "Light");
+
+console.log("printing bag-4--", newBag, bag2);
+
+/**
+ * --- Rules to declare variable
+ * 1. in JS variable must be in camelCase --> thisIsVariableExample
+ * 2. Variable shouldn't be start from any special character and number -->  #price, @price, =price, 56price, 34age
+ *      But we can use _variableName, $variableName
+ * 3. Variables must be self explanatory - price, sumOfNum, age, dob,
+ * 4. Do not use reserved keywords for variables-> function, for, if, this, class, let, const, var, console.
+ * 5. never declare variable with same names
+ * 6. Never use space in between variable name --> new bag
+ * 7. Never use - (hyphen) in variable , new-bag
+ * 8. Never use any special character inside Variable --> new$bag,
+ * 9. JS is case-sensitive language --> NewBag !== newBag
+ *
+ *
+ */
+
+const fortKnight = "class is going on";
+
+console.log("fort---", fortKnight, typeof fortKnight);
+
+const COLOR_RED = "#f00";
+const color_red_light = "f01";
+const COLOR_BLACK = "#000";
+console.log("COLOR_RED---", COLOR_RED, typeof COLOR_RED);
+console.log("color_red_light---", color_red_light, typeof color_red_light);
+console.log("COLOR_BLACK---", COLOR_BLACK, typeof COLOR_BLACK);
+
+// ---------------------- typeof -----------
+/**
+ * typeof will provide the datatype of variable or value
+ * number, string, boolean, undefined, object
+ */
+const age = 50;
+const age2 = "50";
+
+console.log("age--->", age, typeof age); // 50 , number
+console.log("age2--->", age2, typeof age2); // 50 , string
+
+const isRaining = true;
+const isRainingToday = "false";
+
+console.log("isRaining--->", isRaining, typeof isRaining); // true, boolean
+console.log("isRainingToday--->", isRainingToday, typeof isRainingToday); //  false, string
+
+let section = null;
+const section2 = undefined;
+
+console.log("datatype of section ---> ", section, typeof section); // null, object
+console.log("datatype of section2 ---> ", section, typeof section2); // null/undefined  , undefined
+
+section = "null";
+console.log("datatype of section ---> ", section, typeof section); // null, object
