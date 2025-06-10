ac8bce0025d7a512691d0555f6d82c3dce4f2ee3
Vinay Maurya <vinay.shermauryan@gmail.com>
2025-06-10 12:55:20 +0530

    login form theme change, input handle, submission

--- Daily class code/20250609 js DOM-2/script.js
+++ Daily class code/20250609 js DOM-2/script.js
@@ -45,32 +45,125 @@
 
 const mainBody = document.getElementById("main-body");
 
-console.log("mainBody", mainBody);
+const themeBtn = mainBody.querySelector("#theme-btn");
+const mainContainer = mainBody.querySelector("#main-container");
+const subTitle = mainBody.querySelector("#sub-title");
 
-const [h1Tag, h3Tag] = document.getElementsByClassName("abc");
-console.log("h1Tag", h1Tag);
+const handleThemeChange = (event) => {
+  console.log("Theme button clicked");
 
-h1Tag.style.color = "blue";
-h1Tag.style.border = "1px solid blue";
+  const mainBodyColor = window.getComputedStyle(mainBody).backgroundColor;
 
-mainBody.style.backgroundColor = "lightblue";
+  const [userInput, pwInput] = mainBody.querySelectorAll("input");
+  console.log("mainBodyColor", mainBodyColor);
 
-const loginFormContainer = document.getElementById("login-form");
-console.log("loginFormContainer", loginFormContainer);
+  if (mainBodyColor == "#f3f3f3" || mainBodyColor == "rgb(243, 243, 243)") {
+    mainBody.style.backgroundColor = "rgb(43 44 48)";
+    mainContainer.style.backgroundColor = `black`;
+    mainContainer.style.color = `white`;
+    userInput.style.backgroundColor = "#2a2424";
+    userInput.style.color = "white";
+    userInput.style.border = "2px solid white";
+    pwInput.style.backgroundColor = "#2a2424";
+    pwInput.style.color = "white";
+    pwInput.style.border = "2px solid white";
+  } else {
+    mainBody.style.backgroundColor = "#f3f3f3";
+    mainContainer.style.backgroundColor = `#fff `;
+    mainContainer.style.color = `black`;
+    userInput.style.backgroundColor = "white";
+    userInput.style.color = "black";
+    userInput.style.border = "2px solid black";
+    pwInput.style.backgroundColor = "white";
+    pwInput.style.color = "black";
+    pwInput.style.border = "2px solid black";
+  }
+};
+/**
+ * @description Changing theme on button click
+ */
+themeBtn.addEventListener("click", handleThemeChange);
 
-const [userNameField, passwordField] =
-  loginFormContainer.getElementsByTagName("input");
-console.log("inputTags", userNameField, passwordField);
+/**
+ * Form submission
+ * 1. Get values of username and password
+ * 2. Validate the values
+ * 3. If valid then show success message (Green color)
+ * 4. If invalid then show error message (Red color)
+ * 5. Types of validation
+ *    a. Required field
+ *    b. Length of the field
+ *    c. Pattern matching
+ *    d. username should not be same as password
+ *    e. username and password must be greater than 6 and less than 20 characters
+ * 6. Show/Hide Password
+ * 7.
+ *
+ */
 
-userNameField.style.border = "1px solid red";
+const userNameField = mainBody.querySelector("#username");
+const pwField = mainBody.querySelector("#password");
+const submitBtn = mainBody.querySelector("#submit-btn");
+const userError = mainBody.querySelector("#username-error");
+const pwError = mainBody.querySelector("#pw-error");
 
-const btns = document.querySelectorAll("button");
-console.log("btns", btns);
+const userData = { userName: "", password: "" };
 
-const themeBtn = document.querySelector("#theme-btn");
-console.log("themeBtn", themeBtn);
-themeBtn.style.backgroundColor = "red";
+function handleUserName(event) {
+  console.log("printing event", event.target.value);
+  userData.userName = event.target.value;
+}
 
-const [, , , submitBtn] = document.querySelectorAll(".abc");
-console.log("submitBtn", submitBtn);
-submitBtn.style.backgroundColor = "black";
+const handlePassword = (event) => {
+  userData.password = event.target.value;
+};
+
+const handleSubmit = () => {
+  console.log("userData", userData);
+  userError.style.display = "none";
+  pwError.style.display = "none";
+
+  if (!userData.userName) {
+    userError.style.display = "block";
+    userError.innerText = "Username required.";
+    return;
+  } else if (!userData.password) {
+    pwError.style.display = "block";
+    pwError.innerText = "Password required.";
+    return;
+  } else if (userData.userName.length < 6) {
+    userError.style.display = "block";
+    userError.innerText = "Username must be at least 6 characters long.";
+    return;
+  } else if (userData.userName.length > 20) {
+    userError.style.display = "block";
+    userError.innerText = "Username must be less than 20 characters long.";
+    return;
+  } else if (userData.password.length < 6) {
+    pwError.style.display = "block";
+    pwError.innerText = "Password must be at least 6 characters long.";
+    return;
+  } else if (userData.password.length > 20) {
+    pwError.style.display = "block";
+    pwError.innerText = "Password must be less than 20 characters long.";
+    return;
+  } else if (userData.userName == userData.password) {
+    userError.style.display = "block";
+    userError.innerText = "Username and password cannot be the same.";
+    return;
+  } else if (
+    userData.userName.includes(" ") ||
+    userData.password.includes(" ")
+  ) {
+    userError.style.display = "block";
+    userError.innerText = "Username and password cannot contain spaces.";
+    return;
+  }
+
+  handleThemeChange();
+  alert("Form submitted successfully!");
+};
+
+userNameField.addEventListener("change", handleUserName);
+pwField.addEventListener("change", handlePassword);
+submitBtn.addEventListener("click", handleSubmit);
