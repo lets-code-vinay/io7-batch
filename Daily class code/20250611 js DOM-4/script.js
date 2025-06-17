ce9801985d2ac1ede3df8c2cd33866955faa1348
Vinay Maurya <vinay.shermauryan@gmail.com>
2025-06-17 11:26:42 +0530

    dom atm project

--- Daily class code/20250611 js DOM-4/script.js
+++ Daily class code/20250611 js DOM-4/script.js
@@ -1,4 +1,4 @@
-const accUsers = [
+let accUsers = [
   { userName: "Vinay", accNo: "454545", pin: 4545, balance: 45743 },
   { userName: "Chandni", accNo: "787878", pin: 7878, balance: 145147 },
   { userName: "Ajay", accNo: "989898", pin: 9898, balance: 55412 },
@@ -14,9 +14,17 @@ const pinField = mainBody.querySelector("#main-screen-pin");
 const errMsg = mainBody.querySelector(".error-message");
 const welcomeScreen = mainBody.querySelector(".welcome-screen");
 const servicesScreen = mainBody.querySelector(".services-screen");
+const depositScreen = mainBody.querySelector(".deposit-section");
+const withdrawalScreen = mainBody.querySelector(".withdraw-section");
 
 const servicesScreenWCMsg = mainBody.querySelector(".welcome-msg");
-const serviceResponse = mainBody.querySelector(".service-response");
+const balEnqScn = mainBody.querySelector(".balance-enq-screen");
+const balEnq = mainBody.querySelector(".balance-enq");
+
+const depositAmtField = mainBody.querySelector("#deposit-amount");
+const wdField = mainBody.querySelector("#wd-amount");
+
+const enteredAmt = { deposit: 0, withdrawal: 0, transfer: 0 };
 
 console.log("accField-", accField);
 accField.addEventListener("change", handleAcc);
@@ -87,6 +95,126 @@ const handleBalEnq = () => {
   const isUserExist = accUsers.find((user) => user.accNo == enteredAcc);
   console.log("isUserExist--", isUserExist);
 
+  servicesScreen.classList.remove("display-block");
+  servicesScreen.classList.add("display-none");
+
+  balEnqScn.classList.remove("display-none");
+  balEnqScn.classList.add("display-block");
+
   console.log("checking balance", isUserExist?.balance);
-  serviceResponse.innerText = `You current balance is: ${isUserExist?.balance} `;
+  balEnq.innerText = `You current balance is: ${isUserExist?.balance} `;
+};
+
+const handleExitBalEnq = () => {
+  balEnqScn.classList.remove("display-block");
+  balEnqScn.classList.add("display-none");
+
+  servicesScreen.classList.remove("display-none");
+  servicesScreen.classList.add("display-block");
+};
+
+const handleDeposit = () => {
+  servicesScreen.classList.remove("display-block");
+  servicesScreen.classList.add("display-none");
+
+  depositScreen.classList.remove("display-none");
+  depositScreen.classList.add("display-block");
+};
+
+const goToServicesScreen = () => {
+  servicesScreen.classList.remove("display-none");
+  servicesScreen.classList.add("display-block");
+
+  depositScreen.classList.remove("display-block");
+  depositScreen.classList.add("display-none");
+
+  withdrawalScreen.classList.remove("display-block");
+  withdrawalScreen.classList.add("display-none");
+};
+
+const handleExitBtn = () => {
+  welcomeScreen.classList.remove("display-none");
+  welcomeScreen.classList.add("display-block");
+
+  servicesScreen.classList.remove("display-block");
+  servicesScreen.classList.add("display-none");
+
+  depositScreen.classList.remove("display-block");
+  depositScreen.classList.add("display-none");
+
+  withdrawalScreen.classList.remove("display-block");
+  withdrawalScreen.classList.add("display-none");
+};
+
+const handleAmountDeposit = (event) => {
+  enteredAmt.deposit = event.target.value;
+};
+depositAmtField.addEventListener("change", handleAmountDeposit);
+
+const deposit = () => {
+  const { enteredAcc, enteredPin } = enteredDetails;
+  const foundUserIndex = accUsers.findIndex((user) => user.accNo == enteredAcc);
+
+  accUsers = accUsers.map((user, index) => {
+    if (index == foundUserIndex) {
+      return {
+        ...user,
+        balance: Number(user.balance) + Number(enteredAmt.deposit),
+      };
+    } else {
+      return user;
+    }
+  });
+
+  console.log("accUsers--", accUsers);
+  goToServicesScreen();
+};
+
+/**
+ * @description handle Withdrawal to open withdrawal screen
+ */
+const handleWithdraw = () => {
+  servicesScreen.classList.remove("display-block");
+  servicesScreen.classList.add("display-none");
+
+  withdrawalScreen.classList.remove("display-none");
+  withdrawalScreen.classList.add("display-block");
+};
+
+/**
+ * @description Getting withdrawal amt
+ * @param {Object} event
+ */
+const handleWithdrawalAmt = (event) => {
+  enteredAmt.withdrawal = event.target.value;
+};
+
+wdField.addEventListener("change", handleWithdrawalAmt);
+
+/**
+ * @description Withdrawal of amount on click on withdrawal button
+ */
+const withdraw = () => {
+  const foundUserIndex = accUsers.findIndex(
+    (user) => Number(enteredDetails?.enteredAcc) == Number(user?.accNo)
+  );
+  console.log("--", enteredDetails, accUsers);
+
+  accUsers = accUsers.map((user) => {
+    if (Number(enteredDetails?.enteredAcc) == Number(user?.accNo)) {
+      if (enteredAmt?.withdrawal > user?.balance) {
+        errMsg.classList.remove("display-none");
+        errMsg.classList.add("display-block");
+        errMsg.innerText = "You have insufficient balance";
+        return user;
+      } else {
+        return { ...user, balance: user?.balance - enteredAmt?.withdrawal };
+      }
+    } else {
+      return user;
+    }
+  });
+
+  console.log(enteredAmt);
+  goToServicesScreen();
 };
